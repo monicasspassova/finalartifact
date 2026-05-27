c574c8f4b7ffc4a0d526fefebbf5d80bcc3410c7
Irene Casas Ortega <ireneco@cs.washington.edu>
2026-05-27 14:39:13 -0700

    added a puzzle trigger that can be duplicated and changed

---
+++ Assets/Scripts/PuzzleTrigger.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PuzzleTrigger : MonoBehaviour
+{
+    public UIDocument puzzleUI;
+    public int puzzleID;
+    private bool completed = false;
+    private VisualElement root;
+
+    void Start()
+    {
+        root = puzzleUI.rootVisualElement;
+        root.style.display = DisplayStyle.None;
+
+        Button closeBtn = root.Q<Button>();
+        closeBtn.clicked += CloseUI;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !completed)
+        {
+            root.style.display = DisplayStyle.Flex;
+            UnityEngine.Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    public void CloseUI()
+    {
+        root.style.display = DisplayStyle.None;
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void CompletePuzzle()
+    {
+        completed = true;
+        CloseUI();
+    }
+}
